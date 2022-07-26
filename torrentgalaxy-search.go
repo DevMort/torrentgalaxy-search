c2cc8a950c6dd785e6713deba51b5fb8535afb69
empyreum <gitlab@ds7.anonaddy.com>
2022-07-26 21:22:36 +0800

    now shows username, seeds, and size

--- torrentgalaxy-search.go
+++ torrentgalaxy-search.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"strings"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/fatih/color"
 	"github.com/gocolly/colly/v2"
 )
@@ -20,15 +19,16 @@ func main() {
 		"torrentgalaxy.to",
 	))
 
-	c.OnHTML("div.tgxtable", func(e *colly.HTMLElement) {
-		e.DOM.Find("a.txlight").Each(func(_ int, entry *goquery.Selection) {
-			title, _ := entry.Attr("title")
-			if title != "comments" {
-				link, _ := entry.Attr("href")
+	c.OnHTML("div.tgxtablerow.txlight", func(e *colly.HTMLElement) {
+		title, _ := e.DOM.Find("a.txlight").Attr("title")
+		if title != "comments" {
+			link, _ := e.DOM.Find("a.txlight").Attr("href")
+			username := e.DOM.Find("a.username").Text()
+			size := e.DOM.Find("span.badge.badge-secondary.txlight").Text()
+			seeds := e.DOM.Find("font[color=green]").Text()
 
-				fmt.Printf("%s: %s\n%s\n\n", color.GreenString("TITLE"), title, color.YellowString("https://torrentgalaxy.to"+link))
-			}
-		})
+			fmt.Printf("%s: %s (%s, %s, %s)\n%s\n\n", color.GreenString("TITLE"), title, username, color.CyanString(size), color.RedString(seeds), color.YellowString("https://torrentgalaxy.to"+link))
+		}
 	})
 
 	c.Visit("https://torrentgalaxy.to/torrents.php?search=" + strings.ReplaceAll(search_term, " ", "+") + "&sort=seeders&order=desc")
